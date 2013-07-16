3dbdd1a6835ba25adb251088b33ca6ab14bf327c
Jean-Remi Desjardins <jeanremi.desjardins@gmail.com>
2013-07-15 20:21:19 -0700

    Implement a whole bunch of stuff (lot of test cases)

--- src/main/scala/com/github/jedesah/codesheet/api/ScalaCodeSheet.scala
+++ src/main/scala/com/github/jedesah/codesheet/api/ScalaCodeSheet.scala
@@ -9,47 +9,83 @@ import com.github.jedesah.Math
 
 object ScalaCodeSheet {
 
-    def evaluate(AST: Tree, outputResult: List[String], toolBox: ToolBox[reflect.runtime.universe.type], symbols: Set[Tree] = Set()): List[String] = {
-      lazy val classDefs = symbols.filter(_.isInstanceOf[ClassDef]).map(_.asInstanceOf[ClassDef])
-      def updateOutput(newOutput: String) = {
+    def evaluate(AST: Tree, outputResult: List[String], toolBox: ToolBox[reflect.runtime.universe.type], symbols: List[Tree] = Nil): List[String] = {
+      lazy val classDefs: Traversable[ClassDef] = symbols.collect{ case elem: ClassDef => elem}
+      // It's too bad Scala does not support method overriding of definitions
+      // contained within another definition
+      def updateThisOutput(newOutput: String) =
+          updateOutput(outputResult, newOutput)
+      def updateOutput(oldOutput: List[String], newOutput: String) = {
           // -1 because the in memory compiler wraps the code in curly braces to form a block
           // -1 because we are dealing with a zero based collection but 1 based lines in the string
           val index = AST.pos.line - 2
-          val previousOutputOnLine = outputResult(index)
+          val previousOutputOnLine = oldOutput(index)
           // We could conceivably have more than one result on a single line
           val updatedOutput = if (previousOutputOnLine == "") newOutput else previousOutputOnLine + " ; " + newOutput
-          outputResult.updated(index, updatedOutput)
+          oldOutput.updated(index, updatedOutput)
       }
       def evaluateWithSymbols(expr: Tree, extraSymbols: Traversable[Tree] = Set()) = {
           // In Scala 2.10.1, when you create a Block using the following deprecated method, if you pass in only one argument
           // it will be a block that adds a unit expressions at it's end and evaluates to unit. Useless behavior as far as we are concerned.
           // TODO: Remove use of deprecated Block constructor.
           val totalSymbols = symbols ++ extraSymbols
+          println(Block(totalSymbols.toList :+ expr: _*))
           if (totalSymbols.isEmpty) toolBox.eval(expr)
           else toolBox.eval(Block(totalSymbols.toList :+ expr: _*))
       }
       AST match {
         // There is what appears to be a bug in Scala 2.10.1 where you cannot use a capital letter in a case statement with a @
         case ast @ ValDef(_, newTermName, _, assignee) => {
-          val evaluatedAssignement = evaluateWithSymbols(assignee)
-          val output = s"$newTermName = $evaluatedAssignement"
-          updateOutput(output)
+          if (isSimpleExpression(assignee)) outputResult
+          else {
+              val evaluatedAssignement = evaluateWithSymbols(assignee)
+              val output = s"$newTermName = $evaluatedAssignement"
+              updateThisOutput(output)
+          }
         }
-        case expr: Block => expr.children.foldLeft(outputResult) { (result, child) => evaluate(child, result, toolBox, symbols ++ expr.children)}
-        case _ : ClassDef => outputResult
-        case defdef : DefDef => {
-            val sampleValues = assignSampleValues(defdef.vparamss.flatten, defaultSamplePool, classDefs, toolBox)
-            val newOutputResult =
-                if (sampleValues.exists(_.rhs.isEmpty)) outputResult
-                else {
-                    val sampleResult = evaluateWithSymbols(defdef.rhs, sampleValues).toString
-                    val paramList:String = sampleValues.map( valDef => valDef.name + " = " + prettyPrint(valDef.rhs)).mkString(", ")
-                    val lhs:String = defdef.name + (if (paramList.isEmpty) "" else s"($paramList)")
-                    val output = s"$lhs => $sampleResult"
-                    updateOutput(output)
+        // We fold over each child and all of it's preceding childs (inits) and evaluate
+        // it with it's preceding children
+        case expr: Block => expr.children.inits.toList.reverse.drop(1).foldLeft(outputResult) { (result, childs) =>
+            evaluate(childs.last, result, toolBox, symbols ++ childs.init)
+        }
+        case classDef : ClassDef => getConstructor(classDef).flatMap { constructor =>
+            getSampleValuesIfPossible(constructor, defaultSamplePool, classDefs, toolBox).map { sampleValues =>
+                // We remove the value defintions resulting from the class parameters and the primary constructor
+                val body = classDef.impl.body.filter {
+                    case valDef: ValDef => !sampleValues.exists( sampleValDef => sampleValDef.name == valDef.name)
+                    case other => other != constructor
                 }
-            
-            newOutputResult
+                // If there is nothing left (it's of the form case class Foo(bar: String, biz: Int)), don't bother
+                if (body.isEmpty) outputResult
+                else {
+                    val newOutput = body.foldLeft(outputResult) { (result, child) =>
+                        val otherMembers = body.diff(Seq(child))
+                        val allSymbols: List[Tree] = ((symbols ::: sampleValues) :+ classDef) ::: otherMembers
+                        evaluate(child, result, toolBox, allSymbols)
+                    }
+                    if (newOutput == outputResult) outputResult
+                    else {
+                        val signature:String = classDef.name + paramList(sampleValues):String
+                        val output = s"$signature {"
+                        updateOutput(newOutput, output)
+                    }
+                }
+            }
+        } getOrElse {
+            outputResult
+        }
+        case defdef : DefDef => {
+            if (isSimpleExpression(defdef.rhs)) outputResult
+            else {
+                getSampleValuesIfPossible(defdef, defaultSamplePool, classDefs, toolBox) map { sampleValues =>
+                    val sampleResult = evaluateWithSymbols(defdef.rhs, sampleValues :+ defdef).toString
+                    val signature:String = defdef.name + paramList(sampleValues):String
+                    val output = s"$signature => $sampleResult"
+                    updateThisOutput(output)
+                } getOrElse {
+                    outputResult
+                }
+            }
         }
         case _ => {
           val result = evaluateWithSymbols(AST)
@@ -57,18 +93,30 @@ object ScalaCodeSheet {
             case _ : scala.runtime.BoxedUnit => ""
             case _ => result.toString
           }
-          updateOutput(output)
+          updateThisOutput(output)
         }
       }
     }
 
-    
-    def prettyPrint(tree: Tree): String = tree match {
+    // Not sure if I need this anymore
+    /*def prettyPrint(tree: Tree): String = tree match {
       case tree: Apply => tree.fun match {
           case fun: Select => fun.qualifier.toString + "(" + tree.args.mkString(", ") + ")"
           case fun: Ident => fun.toString + "(" + tree.args.mkString(", ") + ")"
       }
       case _ => tree.toString
+    }*/
+
+    def isSimpleExpression(tree: Tree): Boolean = tree match {
+        case _ : Literal => true
+        case tree: Apply => {
+            lazy val composedOfSimple = tree.args.forall(isSimpleExpression(_))
+            tree.fun match {
+                case fun: Select => fun.children.lift(0).map { case _ : New => composedOfSimple case _ => false }.getOrElse(false)
+                case fun: Ident => composedOfSimple
+            }
+        }
+        case _ => false
     }
 
     def computeResults(code: String): List[String] = try {
@@ -89,22 +137,16 @@ object ScalaCodeSheet {
     */
     def assignSampleValues(original: List[ValDef],
                            samplePool: SamplePool,
-                           classDefs: Set[ClassDef],
+                           classDefs: Traversable[ClassDef],
                            toolBox: ToolBox[reflect.runtime.universe.type]): List[ValDef] = {
         def assignCaseClassSampleValue(type_ : Tree): Option[(String, SamplePool)] =
             classDefs.find(_.name.toString == type_.toString).map { classDef =>
-                val isConstructor = (member: Tree) => {
-                    member match {
-                        case defdef: DefDef => defdef.name.toString == "<init>"
-                        case _ => false
-                    }
-                } 
-                val constructor:Option[DefDef] = classDef.impl.body.find(isConstructor).asInstanceOf[Option[DefDef]]
-                constructor.map { constructorDef =>
+                getConstructor(classDef).map { constructorDef =>
                     val innerSamples = assignSampleValues(constructorDef.vparamss.flatten, samplePool, classDefs, toolBox)
                     // TODO: Actually remove sample values from the samplePool instead of just returning it unchanged
                     // TODO: Don't assume it's a case class
                     val sample:String = classDef.name + "(" + innerSamples.map(_.rhs).mkString(",") + ")"
+                    println(sample)
                     (sample, samplePool)
                 }
                 
@@ -121,6 +163,7 @@ object ScalaCodeSheet {
                             tpt.tpt.toString match {
                                 case "List" => samplePool.getList(innerType)
                                 case "Option" => samplePool.getOption(innerType)
+                                case "Seq" => samplePool.getList(innerType)
                                 case _ => None
                             } 
                         }
@@ -136,6 +179,28 @@ object ScalaCodeSheet {
             }
             newValDef :: assignSampleValues(original.tail, newSamplePool, classDefs, toolBox)
         }
+    }
+
+    def getConstructor(classDef: ClassDef): Option[DefDef] =
+      classDef.impl.body.collectFirst{ case defdef: DefDef if defdef.name.toString == "<init>" => defdef }
+    /**
+    * Returns both the value definitions containing the sample values and the string representation of the function signature
+    * if all sample values were correctly generated.
+    */
+    def getSampleValuesIfPossible(defdef: DefDef,
+                                          samplePool: SamplePool,
+                                          classDefs: Traversable[ClassDef],
+                                          toolBox: ToolBox[reflect.runtime.universe.type]): Option[List[ValDef]] = {
+        val sampleValues = assignSampleValues(defdef.vparamss.flatten, samplePool, classDefs, toolBox)
+        if (sampleValues.exists(_.rhs.isEmpty)) None
+        else Some(sampleValues)
+    }
+
+    def paramList(valDefs: List[ValDef]):String = {
+        // Not sure if this is necessary anymore
+        //val list:String = sampleValues.map( valDef => valDef.name + " = " + prettyPrint(valDef.rhs)).mkString(", ")
+        val insideParenthesis = valDefs.map(_.toString.drop(4)).mkString(", ")
+        if (insideParenthesis.isEmpty) "" else s"($insideParenthesis)"
     }
 
     // I don't like the fact that 2 + 3 = 5 which is why I'd rather
