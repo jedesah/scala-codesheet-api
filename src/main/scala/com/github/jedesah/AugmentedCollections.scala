8a920a24b944343a887c2d4cd1bddea0edef0aad
Jean-Remi Desjardins <jeanremi.desjardins@gmail.com>
2013-09-19 12:57:01 -0400

    Remove dead code

--- src/main/scala/com/github/jedesah/AugmentedCollections.scala
+++ src/main/scala/com/github/jedesah/AugmentedCollections.scala
@@ -2,10 +2,6 @@ package com.github.jedesah
 
 package object AugmentedCollections {
 
-	implicit class AugmentedStringList(list: List[String]) {
-		def safeUpdate(index: Int, value: String, padding: String = ""): List[String] =
-			if (index < list.length) list.updated(index, value)
-			else (list :+ padding).safeUpdate(index, value, padding)
 	}
 
 	implicit class AugmentedStream[T](stream: Stream[T]) {
