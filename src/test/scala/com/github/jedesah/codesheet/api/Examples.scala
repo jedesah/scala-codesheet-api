af27f3d0b0a605e56650e8097ba18b080e12e971
Jean-Remi Desjardins <jeanremi.desjardins@gmail.com>
2013-11-21 22:51:50 -0500

    Update and improve Example tests

--- src/test/scala/com/github/jedesah/codesheet/api/Examples.scala
+++ src/test/scala/com/github/jedesah/codesheet/api/Examples.scala
@@ -14,17 +14,19 @@ class Examples extends Specification {
 
 	"Examples" should {
 		"quickSort" in {
-			"1" in {
-				val code = """def quickSort(list: List[Int]): List[Int] = {
-							 |	if (list.isEmpty) Nil
-	    					 |	else {
-	    					 |		val smaller = list.tail.filter(_ < list.head)
-	    					 |		val larger = list.tail.diff(smaller)
-	        				 |		quickSort(smaller) ++ (list.head :: quickSort(larger))
-	    					 |	}
-							 |}""".stripMargin
-				computeResults(code) must beLike { case Result(List(defDef), "") =>
-					defDef must beLike { case DefDefResult("quickSort", List(param), None, body, 1) =>
+			val code = """def quickSort(list: List[Int]): List[Int] = {
+						 |	if (list.isEmpty) Nil
+    					 |	else {
+    					 |		val smaller = list.tail.filter(_ < list.head)
+    					 |		val larger = list.tail.diff(smaller)
+        				 |		quickSort(smaller) ++ (list.head :: quickSort(larger))
+    					 |	}
+						 |}
+						 |
+						 |quickSort(List(3,1,2,6,8,9))""".stripMargin
+			"returns the right programming structure" in {
+				computeResults(code) must beLike { case Result(List(defSort, callSort), "") =>
+					defSort must beLike { case DefDefResult("quickSort", List(param), None, body, 1) =>
 						structureEquals(param, AssignOrNamedArg(Ident(newTermName("list")), Apply(Ident(newTermName("List")), List(Literal(Constant(3)), Literal(Constant(5)), Literal(Constant(7))))))
 						body must beLike { case IfThenElseResult(cond, executed, 2) =>
 							cond ==== SimpleExpressionResult(false, Nil, line = 2)
@@ -35,20 +37,13 @@ class Examples extends Specification {
 							}
 						}
 					}
+					callSort ==== SimpleExpressionResult(List(1,2,3,6,8,9), Nil, 10)
 				}
 			}
-			"1.1" in {
-				val code = """def quickSort(list: List[Int]): List[Int] = {
-							 |	if (list.isEmpty) Nil
-	    					 |	else {
-	    					 |		val smaller = list.tail.filter(_ < list.head)
-	    					 |		val larger = list.tail.diff(smaller)
-	        				 |		quickSort(smaller) ++ (list.head :: quickSort(larger))
-	    					 |	}
-							 |}""".stripMargin
+			"prints correctly to the screen" in {
 				computeResults(code).userRepr must not be empty;
 			}
-			/*"2" in {
+			"slighly more compact version" in {
 				val code = """def quickSort(list: List[Int]): List[Int] = {
 							 |	if (list.isEmpty) Nil
 	    					 |	else {
@@ -62,7 +57,7 @@ class Examples extends Specification {
 						body ==== SimpleExpressionResult(List(3,5,7), Nil, line = 2)
 					}
 				}
-			}*/
+			}.pendingUntilFixed("We don't have great support for pattern matching value definitions quite wet, so this one will have to wait a bit")
 		}
 	}
 }
